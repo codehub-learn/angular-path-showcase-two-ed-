b7012390a5f591010220bafff84db36e8e8a64b2
Ioannis Daniil <jondan97@gmail.com>
2022-11-29 09:08:01 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {PageNotFoundComponent} from "./core/page-not-found/page-not-found.component";
-import {AllUsersComponent} from "./modules/user/all-users.component";
-import {UserDetailsComponent} from "./modules/user-details/user-details.component";
-import {HomeComponent} from "./modules/home/home.component";
-import {DataService} from "./services/data.service";
-import {FooterComponent} from "./core/footer/footer.component";
-import {HeaderComponent} from "./core/header/header.component";
-import { UserRegistrationComponent } from './modules/user-registration/user-registration.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {RxReactiveFormsModule} from "@rxweb/reactive-form-validators";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    PageNotFoundComponent,
-    AllUsersComponent,
-    UserDetailsComponent,
-    FooterComponent,
-    HeaderComponent,
-    UserRegistrationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
     RxReactiveFormsModule
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
